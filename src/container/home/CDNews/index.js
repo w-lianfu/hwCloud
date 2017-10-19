c680ee65d221b0e9c87e9854dc38e244256fd01d
w-lianfu <wlianfu@163.com>
2017-10-19 23:46:25 +0800

    completed home news style

--- src/container/home/CDNews/index.js
+++ src/container/home/CDNews/index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { observer } from 'mobx-react'
 import 'whatwg-fetch'
+import { Icon, Popup } from 'semantic-ui-react'
 
 import Module from  '../../module'
 import Store from './store'
+import './index.scss'
 
 let store = new Store()
 
@@ -19,12 +21,21 @@ class CDNews extends Component {
   }
 
   render() {
-    let list = store.newsList && store.newsList.slice().map(item => (
-      <li key={item.id}>{item.title}</li>
+    let list = store.newsList && store.newsList.slice(0, 3).map(item => (
+      <Link to='/product' key={item.id}><Icon name='circle' />
+        <Popup
+          trigger={<p>{item.title}</p>}
+          content={item.title}
+          inverted
+          size='small'
+          position='bottom left'
+          className='popup'
+        />
+      </Link>
     ))
     return (
-      <section>
-        <ul>{ list }</ul>
+      <section className='cd-news'>
+        <nav>{ list }<Link to='/product'>更多<i>&gt;</i></Link></nav>
       </section>
     )
   }
